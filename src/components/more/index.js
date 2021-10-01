e6dba17c4de2a6faad89c4a7a4592ac9b424c999
Barry de Graaff <info@barrydegraaff.tk>
2021-10-01 08:51:01 +0200

    clean-up

--- src/components/more/index.js
+++ src/components/more/index.js
@@ -1,28 +1,19 @@
 import { createElement } from 'preact';
 import { Text } from 'preact-i18n';
-import { ModalDialog, ActionMenuItem, NakedButton } from '@zimbra-client/components';
+import { ActionMenuItem } from '@zimbra-client/components';
 import { compose } from 'recompose';
 import { withIntl } from '../../enhancers';
-import style from './style';
-import { route } from 'preact-router';
 
 function createMore(props, context) {
    return (
-      <div>
-         <ActionMenuItem onClick={e => handleClick(props, context)}>
-            <Text id={`zimbra-zimlet-ics-export.menuItem`} />
-         </ActionMenuItem>
-      </div>
+      <ActionMenuItem onClick={e => handleClick(props, context)}>
+         <Text id='zimbra-zimlet-ics-export.menuItem' />
+      </ActionMenuItem>
    );
 }
 
 function handleClick(props, context) {
-      window.open("/service/home/~/?auth=co&id="+props.eventToDisplay.id+"&mime=text/plain&noAttach=0&filename="+encodeURIComponent(props.eventToDisplay.name)+".ics");
-   }
+   window.open("/service/home/~/?auth=co&id=" + props.eventToDisplay.id + "&mime=text/plain&noAttach=0&filename=" + encodeURIComponent(props.eventToDisplay.name) + ".ics");
+}
 
-export default compose(
-   withIntl()
-)
-   (
-      createMore
-   )
+export default compose(withIntl())(createMore)
--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 //Load components from Zimbra
 import { createElement, Fragment } from 'preact';
-import { Text } from 'preact-i18n';
 import { provide } from 'preact-context-provider';
-import { withIntl } from './enhancers';
-import { MenuItem, GenericMobileUISidebar, GenericMobileUIToolbar } from '@zimbra-client/components'; // Sidebar/Toolbar are so nav menu is accessible in mobile
-import { Button } from '@zimbra-client/blocks';
-import { useCallback } from 'preact/hooks';
 
 //Load the createMore function from our Zimlet component
 import createMore from './components/more';
@@ -13,14 +8,12 @@ import createMore from './components/more';
 //Create function by Zimbra convention
 export default function Zimlet(context) {
 	//Get the 'plugins' object from context and define it in the current scope
-	const { plugins, store, zimbraBatchClient } = context;
-	const { dispatch, zimletRedux } = store;
+	const { plugins } = context;
 	const exports = {};
 
 	exports.init = function init() {
 		//Here we load the moreMenu Zimlet item into the UI slot:
 		plugins.register('slot::action-menu-event-more', provide(context)(createMore));
 	};
-
 	return exports;
 }
