a2795e3401a904e0dd58ac4e3319fc057fe52102
Barry de Graaff <info@barrydegraaff.tk>
2021-10-20 13:05:30 +0200

    Delete package.sh

--- package.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-npm install
-zimlet build
-zimlet package -v 0.0.1 --zimbraXVersion ">=2.0.0" -n "zimbra-zimlet-ics-export" --desc "Adds a button in the more menu to export an event to ics." -l "Export event as ICS"
