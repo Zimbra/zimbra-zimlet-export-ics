f57cfcfac4e36dda0c6e409e88ec6fc7117dc2e2
Barry de Graaff <info@barrydegraaff.tk>
2021-10-21 11:44:21 +0200

    use useCallback

--- src/components/more/index.js
+++ src/components/more/index.js
@@ -1,18 +1,24 @@
 import { createElement } from 'preact';
+import { useCallback } from 'preact/hooks';
 import { Text } from 'preact-i18n';
 import { ActionMenuItem } from '@zimbra-client/components';
 import { compose } from 'recompose';
 import { withIntl } from '../../enhancers';
 
 function createMore(props, context) {
+   //https://medium.com/@KTAsim/react-performance-event-handlers-using-usecallback-hook-9e4a06f8bb2f
+   const downloadICSHandler = useCallback(() => {
+      downloadICS(props, context)
+   }, [props, context]);
+
    return (
-      <ActionMenuItem onClick={e => handleClick(props, context)}>
+      <ActionMenuItem onClick={downloadICSHandler}>
          <Text id='zimbra-zimlet-ics-export.menuItem' />
       </ActionMenuItem>
    );
 }
 
-function handleClick(props, context) {
+function downloadICS(props, context) {
    window.open(`/service/home/~/?auth=co&id=${encodeURIComponent(props.eventToDisplay.id)}&mime=text/plain&noAttach=0&filename=${encodeURIComponent(props.eventToDisplay.name)}.ics`);
 }
 
